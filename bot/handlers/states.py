4ab85c4eb05c60576fe31531485ae343ab58bbe7
Sapdotten <semenova.a.p.04@gmail.com>
2024-06-30 21:42:33 +0400

    Add states for user form

--- bot/handlers/states.py
+++ bot/handlers/states.py
@@ -1,13 +1,8 @@
 from aiogram.fsm.state import State, StatesGroup
 
-
 class UserFormState(StatesGroup):
     FIO = State()
     VIN = State()
     PHONE_NUMBER = State()
     PROBLEM = State()
-    CHECK = State()
-    CHANGE_FIO = State()
-    CHANGE_PHONE_NUMBER = State()
-    CHANGE_VIN = State()
-    CHANGE_PROBLEM = State()
+    
