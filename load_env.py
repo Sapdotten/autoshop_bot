cf80293f1205eedb6252a84ff1e32ed9d2939be9
Sapdotten <semenova.a.p.04@gmail.com>
2024-07-01 12:41:43 +0400

    try to fix conflicts

--- load_env.py
+++ load_env.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+<<<<<<< HEAD
 
 def load_environ():
     dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -8,3 +9,11 @@ def load_environ():
 
 
 # print(os.environ.get('API_KEY'))
+=======
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+
+print(os.environ.get('API_KEY'))
+>>>>>>> 465acad (Start of developing)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+<<<<<<< HEAD
 
 from aiogram import Bot, Dispatcher
 from bot.handlers import request
@@ -11,12 +12,21 @@ from load_env import load_environ
 logging.basicConfig(level=logging.INFO)
 def register_routers(dp):
     dp.include_routers(request.command_router)
+=======
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+
+
+>>>>>>> 465acad (Start of developing)
 
 
 async def main() -> None:
     """
     Entry point
     """
+<<<<<<< HEAD
     # load_environ()
     
     session = AiohttpSession()
@@ -29,6 +39,19 @@ async def main() -> None:
         await dp.start_polling(bot)
     except Exception as ex:
         print(ex)
+=======
+    load_dotenv('.env')
+    token = os.environ.het("API_KEY")
+    bot = Bot(token)
+    storage = MemoryStorage()
+    dp = Dispatcher(bot, storage=storage)
+
+    try:
+        await dp.skip_updates()
+        await dp.start_polling()
+    except Exception as _ex:
+        print(_ex)
+>>>>>>> 465acad (Start of developing)
 
 
 if __name__ == "__main__":
